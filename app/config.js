2bc15ad3cecac883a70f6d64e162d000d215b721
Kiril Knysh <k.knysh@gmail.com>
2017-01-18 23:14:30 +0100

    Initial implementation backbone;

---
+++ app/config.js
@@ -0,0 +1,9 @@
+exports.wsServerConfig = {
+    uri: 'ws://localhost:81/zandbak'
+};
+
+exports.zandbakConfig = {
+    sand: 'lodash',
+    workersCount: 2,
+    maxWorkersCount: 5,
+};
--- app/zandbak-service.js
+++ app/zandbak-service.js
@@ -1,20 +1,58 @@
+const WebSocketClient = require('uws');
+
 const zandbak = require('zandbak');
 
+const { wsServerConfig, zandbakConfig } = require('./config');
+
+
+console.log('[zandbak-service]', 'ws connection to', wsServerConfig.uri);
+console.log('[zandbak-service]', 'zandbak sand', zandbakConfig.sand);
+
+const wsClient = new WebSocketClient(wsServerConfig.uri);
 const sandbox = zandbak({
     zandbakOptions: {
-        workersCount: 2,
-        maxWorkersCount: 5,
+        workersCount: zandbakConfig.workersCount,
+        maxWorkersCount: zandbakConfig.maxWorkersCount,
         logs: '-error,-warn,-log,+perf',
     },
     eAppOptions: {
         showDevTools: false,
         browserWindow: { width: 400, height: 400, show: false },
         urlOptions: { userAgent: '_qd-ua' },
-        sand: 'lodash',
+        sand: zandbakConfig.sand,
         logs: '-error,-warn,-log',
     }
 });
 
-sandbox.resetWith(null);
+function destroy() {
+    wsClient.close();
+    sandbox.destroy();
+}
 
-setTimeout(sandbox.destroy, 2000);
+wsClient
+    .on('open', () => {
+        console.log('[zandbak-service]', 'ws connection is opened');
+    })
+    .on('error', () => {
+        // TODO: why there is no error as a parameter???
+        console.error('[zandbak-service]', 'ws error');
+    })
+    .on('message', (message) => {
+        const { type, payload } = message;
+
+        switch (type) {
+            case 'resetWith':
+                return sandbox.resetWith(payload);
+            case 'exec':
+                return sandbox.exec(payload);
+            case 'destroy':
+                return destroy();
+            default:
+                return console.warn('[zandbak-service]', 'unknown message from sw server');
+        }
+    });
+
+sandbox.on('solved', (task, error, result) => {
+    // TODO: EPIPE, mother fucker!
+    wsClient.send({ task, error, result });
+});
